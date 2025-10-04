e65e667985d68366b3c388516c6db900447d26c4
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2025-10-04 00:16:07 -0300

    Atualizar o Routes.js
    adicionando comentários nos end point

--- Routes.js
+++ Routes.js
@@ -3,9 +3,32 @@ const contatoController = require('../controllers/contatoController');
 
 const router = express.Router();
 
+/**
+ * @route   POST /api/contatos
+ * @desc    Adicionar um novo contato na api
+ * @access  Public
+ */
 router.post('/contatos', contatoController.adicionarContato);
+
+/**
+ * @route GET /api/listar contatos 
+ * @desc   listar um contato existente 
+ * @access  Public
+ */
 router.get('/contatos', contatoController.listarContatos);
+
+/**
+ * @route Patch /api/atualizarContato 
+ * @desc    Atualizar um contato existente por   id 
+ * @access  Public
+ */
 router.patch('/contatos/:id', contatoController.atualizarContato);
+
+/**
+ * @route   DELETE /api/excluirContato 
+ * @desc    excluir um contato por id 
+ * @access  Public
+ */
 router.delete('/contatos/:id', contatoController.excluirContato);
 
 module.exports = router;
